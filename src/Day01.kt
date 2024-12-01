4ea59fb5237a2a17b4f43d07e4fcf05560bac799
certux <certux@gmx.de>
2024-12-01 10:47:30 +0100

    Solution for day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,40 @@
+import kotlin.math.abs
+
 fun main() {
+    fun inputToIntLists(input:List<String>) =
+        input.map { inputList ->
+            inputList.split("""\s+""".toRegex()).let { Pair(it[0].toInt(), it[1].toInt())}
+        }.unzip()
+
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val (firstList, secondList) = inputToIntLists(input)
+
+        val distance = firstList.sorted().zip(secondList.sorted()).sumOf { abs(it.first - it.second) }
+        return distance
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (firstList, secondList) = inputToIntLists(input)
+
+        val occurrencesInSecondList = mutableMapOf<Int, Int>()
+
+        val result = firstList.sumOf { v1 ->
+            val occurrences = occurrencesInSecondList.getOrPut(v1) {
+                secondList.count { v2 -> v2 == v1 }
+            }
+            v1 * occurrences
+        }
+        return result
+
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    check(part1(listOf("1  2", "3 3")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    //val testInput = readInput("Day01_test")
+    //check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
